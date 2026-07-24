87d79cc24ba0cb5d8b43fb0d1cb1b459e50e9faf
Candido25 <115950054+Candido25@users.noreply.github.com>
2026-07-24 16:09:47 -0500

    Renovar caché para la observación de losa nervada

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "mi-casa-segura-pwa-v12";
+const CACHE_VERSION = "mi-casa-segura-pwa-v13";
 const APP_SHELL = [
   "/",
   "/index.html",
@@ -18,6 +18,7 @@ const APP_SHELL = [
   "/hero.css",
   "/site-global.js",
   "/recubrimiento-observation.js?v=20260723-1",
+  "/losa-nervada-observation.js?v=20260724-1",
   "/script.js",
   "/site.webmanifest",
   "/app-icon-192.png",
